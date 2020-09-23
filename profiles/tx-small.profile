46c36862f0bb72aa0b5a4fdbda74f1872282b89a
EagleoutIce <maeusepiep99@web.de>
2020-09-23 02:34:25 +0200

    Initial commit with setup source files etc. Readme is empty (for now)
    Signed-off-by: EagleoutIce <maeusepiep99@web.de>

---
+++ profiles/tx-small.profile
@@ -0,0 +1,41 @@
+# See: https://tug.org/texlive/doc/install-tl.html#PROFILES
+# texlive.profile written on Tue Sep 22 22:12:31 2020 UTC
+# It will NOT be updated and reflects only the
+# installation profile at installation time.
+selected_scheme scheme-custom
+TEXDIR /opt/texlive/texdir
+TEXMFCONFIG $TEXMFSYSCONFIG
+TEXMFHOME $TEXMFLOCAL
+TEXMFLOCAL /opt/texlive/texdir/texmf-local
+TEXMFSYSCONFIG /opt/texlive/texdir/texmf-config
+TEXMFSYSVAR /opt/texlive/texdir/texmf-var
+TEXMFVAR $TEXMFSYSVAR
+binary_x86_64-linux 1
+binary_x86_64-linuxmusl 1
+collection-basic 1
+collection-fontsrecommended 1
+collection-fontutils 1
+collection-langgerman 1
+collection-latex 1
+collection-latexrecommended 1
+collection-mathscience 1
+collection-metapost 1
+collection-pictures 1
+instopt_adjustpath 0
+instopt_adjustrepo 1
+instopt_letter 0
+instopt_portable 0
+instopt_write18_restricted 1
+tlpdbopt_autobackup 1
+tlpdbopt_backupdir tlpkg/backups
+tlpdbopt_create_formats 1
+tlpdbopt_desktop_integration 0
+tlpdbopt_file_assocs 0
+tlpdbopt_generate_updmap 0
+tlpdbopt_install_docfiles 0
+tlpdbopt_install_srcfiles 0
+tlpdbopt_post_code 1
+tlpdbopt_sys_bin /usr/local/bin
+tlpdbopt_sys_info /usr/local/share/info
+tlpdbopt_sys_man /usr/local/share/man
+tlpdbopt_w32_multi_user 0
---
+++ setup-docker.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+# TODO: install sltx aswell, as soon as it can compile :D
+
+# Exit immediately if command nonzero
+set -eu
+
+# Desired profile for container
+TARGET_PROFILE="$1"
+
+echo "Running setup for target-profile \"${TARGET_PROFILE}\""
+
+echo " - Install Packages"
+# Install wanted packages
+apk add --update --no-cache bash python3 tar wget gnupg ghostscript perl
+echo " - Setup python"
+ln -sf python3 /usr/bin/python
+python3 -m ensurepip
+pip3 install --no-cache --upgrade pip setuptools
+
+echo " - Preparing the texlive installation"
+# Install texlive itself
+WOKRING_DIR=/tmp/install
+TX_UNZIP=installer
+mkdir -p ${WOKRING_DIR}/${TX_UNZIP}
+cd ${WOKRING_DIR}
+
+# Retrieve the ascii armor
+echo "   - Retrieving armor from texlive"
+wget -q https://tug.org/texlive/files/texlive.asc
+gpg --no-default-keyring --keyring trustedkeys.kbx --import texlive.asc
+
+# We need a link to get the newest install-tl from:
+MIRROR_URL=$(wget -qS --spider --tries 6 http://mirror.ctan.org/ 2>&1 | sed -ne 's/.*Location: \(\w*\)/\1/p' | head -n 1)
+echo "   - Using mirror ${MIRROR_URL}"
+
+echo " - Starting download"
+download_tl() {
+    final_url="${MIRROR_URL}systems/texlive/tlnet/install-tl-unx.tar.gz$1"
+    echo "   - Trying to download \"${final_url}\""
+    wget -nv --tries 6 "${final_url}"
+}
+download_tl ""
+download_tl ".sha512" # get the sha
+download_tl ".sha512.asc"
+
+# Checks
+gpgv ./install-tl-unx.tar.gz.sha512.asc ./install-tl-unx.tar.gz.sha512
+sha512sum -c ./install-tl-unx.tar.gz.sha512
+
+tar --strip-components 1 -zxf ./install-tl-unx.tar.gz -C ${WOKRING_DIR}/${TX_UNZIP}
+cd ${TX_UNZIP}
+
+# Run the installer
+./install-tl -profile=/profiles/${TARGET_PROFILE}.profile
+
+# install additional packages, if present
+PACKAGE_LOC="profiles/${TARGET_PROFILE}.packages"
+if [ -f "${PACKAGE_LOC}" ]; then
+    AD_PACKAGES=$(cat ${PACKAGE_LOC})
+    if [ -n ${AD_PACKAGES} ]; then
+        echo " - Installing additional packages: \"${AD_PACKAGES}\""
+        tlmgr install ${AD_PACKAGES}
+    fi
+fi
+
+echo "Cleaning up afterwards to get a smaller container"
+# We clean doc and source to, if some profile wants to install them anyway
+rm -rf \
+  /opt/texlive/texdir/install-tl /opt/texlive/texdir/install-tl.log \
+  /opt/texlive/texdir/texmf-dist/doc /opt/texlive/texdir/texmf-dist/source \
+  /opt/texlive/texdir/texmf-var/web2c/tlmgr.log \
+  /root/.gnupg \
+  /setup-docker.sh \
+  /profiles/ \
+  /texlive.asc \
+  ${WOKRING_DIR}
