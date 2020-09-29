e58f697877dc47eb5f82ae37ede9d9fe19c20ed5
EagleoutIce <maeusepiep99@web.de>
2020-09-29 15:29:03 +0200

    Updated the package list for default, using updated sltx
    Signed-off-by: EagleoutIce <maeusepiep99@web.de>

--- profiles/tx-default.sh
+++ profiles/tx-default.sh
@@ -1,7 +1,7 @@
-tlmgr install latexmk biblatex biber bibtex8 xindy texliveonfly texdef texloganalyser \
-    academicons anonymouspro aurical barcodes calligra calligra-type1 crimson crimsonpro \
-    dejavu ebgaramond ebgaramond-maths euxm fontawesome fourier initials kpfonts \
-    montserrat sourcecodepro xcharter overlock ly1 biblatex-ieee
+tlmgr install latexmk biblatex biber bibtex8 xindy texliveonfly \
+    anonymouspro aurical calligra calligra-type1 crimson crimsonpro \
+    dejavu ebgaramond ebgaramond-maths euxm fontawesome initials kpfonts \
+    montserrat sourcecodepro xcharter overlock ly1 CormorantGaramond
 pip install sltx
 # TODO: pip should install all dependencies and setup texmf => get lithie-util
 sltx dependency /sltx-dep.yaml
