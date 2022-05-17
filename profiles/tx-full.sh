2e3ff4aeb38a80fd6c0c01caee83740ec2f82feb
Florian Sihler <florian.sihler@uni-ulm.de>
2022-05-17 22:05:57 +0200

    get fira code from testing

--- profiles/tx-full.sh
+++ profiles/tx-full.sh
@@ -1,3 +1,5 @@
 apk add --no-cache file imagemagick
-apk add --update --no-cache font-fira-code
+# font fira code is in edge testing
+echo "@testing http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
+apk add --update --no-cache font-fira-code@testing
 sed -i '71 c<policy domain="coder" rights="all" pattern="MVG" />' /etc/ImageMagick-7/policy.xml
